5f93844de69b402ad96b8bf6c3ff9238082f9a05
Shay Kreymer <kreymershay@gmail.com>
2021-07-24 16:16:01 +0300

    Implementation of GMM

--- main.py
+++ main.py
@@ -7,27 +7,67 @@ Created on Fri Jul 23 20:03:45 2021
 
 import numpy as np
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 import utils
 
-np.random.seed(10)
+np.random.seed(1)
 
-L = 10
-N = 1000
-gamma = 0.4
+L = 15
+N = 100000
+gamma = 0.2
 
 x = np.random.rand(L)
-x = x / np.linalg.norm(x)
+x = 10 * x / np.linalg.norm(x)
 y_clean = utils.generate_micrograph_1d(x, gamma, L, N)
 
-SNR = 50
-sigma2 = 1 / (L * SNR)
+SNR = 500
+sigma2 = (np.linalg.norm(x) ** 2) / (L * SNR)
 y = y_clean + np.random.normal(loc=0, scale=np.sqrt(sigma2), size=np.shape(y_clean))
 
 shifts_2nd = utils.shifts_2nd(L)
 
-shifts_3rd = utils.shifts_3rd(L)
+shifts_3rd = utils.shifts_3rd_reduced(L)
 
-print(utils.ac3(y_clean, shifts_3rd[2][0], shifts_3rd[2][1]) / utils.ac3(x, shifts_3rd[2][0], shifts_3rd[2][1]))
+ac1_y = utils.ac1(y)
 
+ac2_y = np.zeros((len(shifts_2nd), ))
+for (i, shift) in enumerate(shifts_2nd):
+    ac2_y[i] = utils.ac2(y, shift)
+    
+ac3_y = np.zeros((len(shifts_3rd), ))
+for (i, shifts) in enumerate(shifts_3rd):
+    ac3_y[i] = utils.ac3(y, shifts[0], shifts[1])
+
+L2 = len(shifts_2nd)
+L3 = len(shifts_3rd)
+W = np.eye(1 + L2 + L3)
+
+gamma0 = 0.195
+x0 = np.random.rand(L)
+x0 = (np.linalg.norm(x0) ** 2) * x0 / np.linalg.norm(x0)
+
+x_gamma0 = np.concatenate((x0, np.array([gamma0])))
+
+estimation = utils.opt(x_gamma0, ac1_y, ac2_y, ac3_y, shifts_2nd, shifts_3rd, sigma2, W)
+x_est = estimation.x[ :L]
+gamma_est = estimation.x[-1]
+
+err_est = utils.calc_err(x, x_est)
+
+print(f'The error for the MoM is {err_est}.')
+
+samples = utils.sample(y, L)
+
+f_gmm = utils.calc_function_gmm(samples, gamma0, x0, shifts_2nd, shifts_3rd, sigma2)
+cov_f = np.cov(f_gmm)
+W_gmm = np.linalg.inv(cov_f)
+W_gmm = W_gmm / np.sum(W_gmm)
+
+estimation_gmm = utils.opt(x_gamma0, ac1_y, ac2_y, ac3_y, shifts_2nd, shifts_3rd, sigma2, W_gmm)
+x_est_gmm = estimation_gmm.x[ :L]
+gamma_est_gmm = estimation_gmm.x[-1]
+
+err_est_gmm = utils.calc_err(x, x_est_gmm)
+
+print(f'The error for the GMM is {err_est_gmm}.')
--- utils.py
+++ utils.py
@@ -9,6 +9,9 @@ import numpy as np
 
 import itertools
 
+from scipy.optimize import minimize
+
+
 def generate_micrograph_1d(x, gamma, L, N):
     y = np.zeros((N, ))
     
@@ -45,6 +48,18 @@ def ac2(z, shift):
     z_shifted = z_shifted[ :l]
     return np.sum(z * z_shifted) / l
 
+def dac2(z, shift):
+    l = len(z)
+    z_shifted = np.zeros((l + shift, ))
+    z_shifted[shift: shift + l] = z
+    z_tmp = np.zeros((l + shift, ))
+    z_tmp[ :l] = z
+    z = z_tmp
+    dac2 = np.zeros((l, ))
+    for i in range(l):
+        dac2[i] = (z_shifted[i] + z[i + shift]) / l
+    return dac2
+
 def ac3(z, shift1, shift2):
     l = len(z)
     z_shifted1 = np.zeros((l + shift1, ))
@@ -55,18 +70,108 @@ def ac3(z, shift1, shift2):
     z_shifted2 = z_shifted2[ :l]
     return np.sum(z * z_shifted1 * z_shifted2) / l
 
+def dac3(z, shift1, shift2):
+    l = len(z)
+    z_shifted1 = np.zeros((l + np.maximum(shift1, shift2), ))
+    z_shifted1[shift1: shift1 + l] = z
+    z_shifted2 = np.zeros((l + np.maximum(shift1, shift2), ))
+    z_shifted2[shift2: shift2 + l] = z
+    z_tmp = np.zeros((l + np.maximum(shift1, shift2), ))
+    z_tmp[ :l] = z
+    z = z_tmp
+    dac3 = np.zeros((l, ))
+    for i in range(l):
+        dac3[i] = (z_shifted1[i] * z_shifted2[i] + z[i + shift1] * z_shifted2[i + shift1] + z[i + shift2] * z_shifted1[i + shift2]) / l
+    return dac3
+
 def shifts_2nd(L):
     return list(np.arange(L))
 
 def shifts_3rd(L):
     return list(itertools.product(np.arange(L), np.arange(L)))
 
-def calc_g_dg(ac1_y, ac2_y, ac3_y, gamma, x, shifts_2nd, shifts_3rd):
-    g = np.zeros((1 + len(shifts_2nd) + len(shifts_3rd), ))
+def shifts_3rd_reduced(L):
+    return list(itertools.combinations(np.arange(L), r=2)) + [(np.arange(L)[i], np.arange(L)[i]) for i in range(L)]
+
+def sample(y, L):
+    samples = np.zeros((L, len(y) - L + 1))
+    for i in range(len(y) - L + 1):
+        samples[ :, i] = y[i: i + L]
+    return samples
+
+def calcM1(samples):
+    return samples[0, :]
+
+def calcM2(samples, shift):
+    return samples[0, :] * samples[shift, :]
+
+def calcM3(samples, shift1, shift2):
+    return samples[0, :] * samples[shift1, :] * samples[shift2, :]
+
+def calc_function_gmm(samples, gamma, x, shifts_2nd, shifts_3rd, sigma2):
+    Nsamples = np.shape(samples)[1]
+    L2 = len(shifts_2nd)
+    L3 = len(shifts_3rd)
+    f_gmm = np.zeros((1 + L2 + L3, Nsamples))
+    f_gmm[0, :] = calcM1(samples) - gamma * ac1(x)
+    for (i, shift) in enumerate(shifts_2nd):
+        f_gmm[1 + i, :] = calcM2(samples, shift) - gamma * ac2(x, shift)
+        if shift == 0:
+            f_gmm[1 + i, :] -= sigma2
+    for (i, shifts) in enumerate(shifts_3rd):
+        f_gmm[1 + L2 + i, :] = calcM3(samples, shifts[0], shifts[1]) - gamma * ac3(x, shifts[0], shifts[1])
+        if shifts[0] == 0:
+            f_gmm[1 + L2 + i, :] -= sigma2 * gamma * ac1(x)
+        if shifts[1] == 0:
+            f_gmm[1 + L2 + i, :] -= sigma2 * gamma * ac1(x)
+        if shifts[0] == shifts[1]:
+            f_gmm[1 + L2 + i, :] -= sigma2 * gamma * ac1(x)
+    return f_gmm
+    
+def calc_g_dg(ac1_y, ac2_y, ac3_y, gamma, x, shifts_2nd, shifts_3rd, sigma2):
+    L = len(x)
+    L2 = len(shifts_2nd)
+    L3 = len(shifts_3rd)
+    g = np.zeros((1 + L2 + L3, ))
+    dg = np.zeros((L + 1, 1 + L2 + L3))
     g[0] = ac1_y - gamma * ac1(x)
+    dg[:L, 0] = - gamma / L
+    dg[-1, 0] = - ac1(x)
     for (i, shift) in enumerate(shifts_2nd):
         g[1 + i] = ac2_y[i] - gamma * ac2(x, shift)
+        dg[ :L, 1 + i] = - gamma * dac2(x, shift)
+        dg[-1, 1 + i] = - ac2(x, shift)
+        if shift == 0:
+            g[1 + i] -= sigma2
     for (i, shifts) in enumerate(shifts_3rd):
-        g[1 + len(shifts_2nd) + i] = ac3_y[i] - gamma * ac3(x, shifts[0], shifts[1])
-    return g
+        g[1 + L2 + i] = ac3_y[i] - gamma * ac3(x, shifts[0], shifts[1])
+        dg[ :L, 1 + L2 + i] = - gamma * dac3(x, shifts[0], shifts[1])
+        dg[-1, 1 + L2 + i] = - ac3(x, shifts[0], shifts[1])
+        if shifts[0] == 0:
+            g[1 + L2 + i] -= sigma2 * gamma * ac1(x)
+            dg[ :L, 1 + L2 + i] -= sigma2 * gamma / L
+            dg[-1, 1 + L2 + i] -= sigma2 * ac1(x)
+        if shifts[1] == 0:
+            g[1 + L2 + i] -= sigma2 * gamma * ac1(x)
+            dg[ :L, 1 + L2 + i] -= sigma2 * gamma / L
+            dg[-1, 1 + L2 + i] -= sigma2 * ac1(x)
+        if shifts[0] == shifts[1]:
+            g[1 + L2 + i] -= sigma2 * gamma * ac1(x)
+            dg[ :L, 1 + L2 + i] -= sigma2 * gamma / L
+            dg[-1, 1 + L2 + i] -= sigma2 * ac1(x)
+    return g, dg
+
+def calc_f_df(x_gamma, ac1_y, ac2_y, ac3_y, shifts_2nd, shifts_3rd, sigma2, W):
+    gamma = x_gamma[-1]
+    x = x_gamma[ :-1]
+    g, dg = calc_g_dg(ac1_y, ac2_y, ac3_y, gamma, x, shifts_2nd, shifts_3rd, sigma2)
+    f = g @ W @ g
+    df = 2 * dg @ W @ g
+    return f, df
+
+def opt(x_gamma0, ac1_y, ac2_y, ac3_y, shifts_2nd, shifts_3rd, sigma2, W, gtol=1e-12):
+    return minimize(fun=calc_f_df, x0=x_gamma0, method='BFGS', jac=True, options={'disp':True, 'gtol': gtol}, args = (ac1_y, ac2_y, ac3_y, shifts_2nd, shifts_3rd, sigma2, W))
+
+def calc_err(x, x_est):
+    return np.linalg.norm(x - x_est) / np.linalg.norm(x)
     
