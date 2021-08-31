f1c875aae6555ea02773f0b5cf0f1919f828265a
Shay Kreymer <56560602+krshay@users.noreply.github.com>
2021-08-31 20:06:01 +0300

    Update experiment_size.py

--- experiment_size.py
+++ experiment_size.py
@@ -16,9 +16,11 @@ import utils
 
 import scipy.optimize as optimize
 
+import shelve
+
 np.random.seed(1)
 
-L = 7
+L = 21
 gamma = 0.2
 SNR = 50
 
@@ -27,7 +29,7 @@ shifts_3rd = utils.shifts_3rd_reduced(L)
 
 gamma0 = 0.18
 x_gamma0s = []
-Nguesses = 10
+Nguesses = 5
 for i in range(Nguesses):
     x0 = np.random.rand(L)
     x0 = x0 / np.linalg.norm(x0)
@@ -35,10 +37,10 @@ for i in range(Nguesses):
     x_gamma0 = np.concatenate((x0, np.array([gamma0])))
     x_gamma0s.append(x_gamma0)
 
-Nsizes = 8
-sizes = np.logspace(4, 6, num=Nsizes)
+Nsizes = 10
+sizes = np.logspace(4, 7.5, num=Nsizes)
 
-Niters = 50
+Niters = 20
 xs = []
 for it in range(Niters):
     x = np.random.rand(L)
@@ -55,14 +57,16 @@ results_final_mom = np.zeros((Niters, Nsizes), dtype=optimize.optimize.OptimizeR
 results_final_gmm = np.zeros((Niters, Nsizes), dtype=optimize.optimize.OptimizeResult)
 times_final_mom = np.zeros((Niters, Nsizes, Nguesses))
 times_final_gmm = np.zeros((Niters, Nsizes, Nguesses))
-for (idx, N) in enumerate(sizes):
-    N = int(N)
-    print(N)
-    for it in range(Niters):
-        x = xs[it]
+for it in range(Niters):
+    print(it)
+    x = xs[it]
+    for (idx, N) in enumerate(sizes):
+        N = int(N)
+        print(N)
+
         y_clean = utils.generate_micrograph_1d(x, gamma, L, N)
         y = y_clean + np.random.normal(loc=0, scale=np.sqrt(sigma2), size=np.shape(y_clean))
-        
+        del y_clean
         ac1_y = utils.ac1(y)
         ac2_y = np.zeros((len(shifts_2nd), ))
         for (i, shift) in enumerate(shifts_2nd):
@@ -70,9 +74,11 @@ for (idx, N) in enumerate(sizes):
         ac3_y = np.zeros((len(shifts_3rd), ))
         for (i, shifts) in enumerate(shifts_3rd):
             ac3_y[i] = utils.ac3(y, shifts[0], shifts[1])
+        
 
         W = utils.calc_W_heuristic(shifts_2nd, shifts_3rd)
         samples = utils.sample(y, L)
+        del y
         estimations_mom = []
         estimations_gmm = []
         for i in range(Nguesses):
@@ -83,16 +89,16 @@ for (idx, N) in enumerate(sizes):
             
             start = time.time()
             f_gmm = utils.calc_function_gmm(samples, x_gamma0s[i][-1], x_gamma0s[i][:-1], shifts_2nd, shifts_3rd, sigma2)
-            cov_f = np.cov(f_gmm)
+            cov_f = np.cov(np.sqrt(N) * f_gmm)
             W_gmm = np.linalg.inv(cov_f)
             W_gmm = W_gmm
             estimation_gmm = utils.opt(x_gamma0s[i], ac1_y, ac2_y, ac3_y, shifts_2nd, shifts_3rd, sigma2, W_gmm)
             times_gmm[it, idx, i] = time.time() - start
             estimations_gmm.append(estimation_gmm)
+        del samples
+        del f_gmm
         results_mom[it, idx, :] = estimations_mom
         results_gmm[it, idx, :] = estimations_gmm
-        
-del y, samples, f_gmm
 
 for (idx, _) in enumerate(sizes):
     for it in range(Niters):
@@ -129,7 +135,7 @@ with plt.style.context('ieee'):
     fig = plt.figure()
     plt.loglog(sizes, np.mean(errs_mom, axis=0), label=r'Method of Moments', lw=2)
     plt.loglog(sizes, np.mean(errs_gmm, axis=0), label=r'Generalized Method of Moments', lw=2)
-    plt.loglog(sizes, np.mean(errs_mom, axis=0)[0]*(sizes/sizes[0])**(-1/2), 'k--', lw=1)    
+    plt.loglog(sizes, np.mean(errs_mom, axis=0)[0]*(sizes/sizes[0])**(-1/2), 'k--', lw=1)       
     plt.legend(loc=1)
     plt.xlabel('Measurement length [pixels]')
     plt.ylabel('Mean estimation error')
@@ -144,3 +150,18 @@ with plt.style.context('ieee'):
     plt.ylabel('Mean number of iterations')
     fig.tight_layout()
     plt.show()
+
+filename='/home/shaykreymer/Documents/MTD-GMM/DATA//shelve_new.out'
+my_shelf = shelve.open(filename,'n') # 'n' for new
+
+for key in dir():
+    try:
+        my_shelf[key] = globals()[key]
+    except TypeError:
+        #
+        # __builtins__, my_shelf, and imported modules can not be shelved.
+        #
+        print('ERROR shelving: {0}'.format(key))
+    except AttributeError:
+        print('ERROR shelving: {0}'.format(key))
+my_shelf.close()
